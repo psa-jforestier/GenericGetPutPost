67ccab58239473752e31791d4d78e69f2ba9e052
psa-jforestier <psa-jforestier@xy.z>
2026-01-17 21:43:39 +0000

    simple CORS handling, waiting for better code

--- php/ggpp.php
+++ php/ggpp.php
@@ -9,6 +9,10 @@ $posted_data = trim(@file_get_contents('php://input'));
 $client_id = (@$_SERVER['HTTP_X_CLIENT_ID'] != '' ? $_SERVER['HTTP_X_CLIENT_ID'] : @$_REQUEST['client_id']);
 $client_id = trim($client_id);
 
+    header('Access-Control-Allow-Origin: *'); // Allow access from any origin
+
+
+
 // client_id is required (and must be authorized)
 if ($client_id == '') {
     DIE_WITH_ERROR(400, 'Missing client_id');
@@ -35,6 +39,10 @@ $allowed = $ggpp->check_rate_limit($client_id, $md5_ip_address, $client_config['
 if (!$allowed) {
     DIE_WITH_ERROR(429, 'Too Many Requests');
 }
+
+
+
+
 
 if ($http_method == 'PUT') {
     // create a new document
@@ -85,6 +93,8 @@ else if ($http_method == 'OPTIONS') {
     header('Access-Control-Allow-Methods: GET, PUT, POST, OPTIONS');
     header('Access-Control-Allow-Headers: X-Client-Id, Content-Type');
     header('Access-Control-Max-Age: 86400'); // cache for 1 day
+    header('Access-Control-Allow-Origin: *'); // Allow access from any origin
+    exit();
     echo "client_id: $client_id\n";
     echo "rate limit: ".$client_config['max_req_count']." requests per ".$client_config['max_req_period']." seconds\n";
     echo "rate usage: ".$ggpp->get_rate_usage($client_id, $md5_ip_address, $client_config['max_req_period'], $client_config['max_req_count'], $client_config['use_ip_lock'])." requests used in the current period\n";
